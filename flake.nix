e41bde52752febfbabbf0ee86337aa24325d02f4
Uzair Mohammed <uzair.hamed@gmail.com>
2026-04-12 15:56:31 -0700

    Initialized Nix environment

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A Nix-flake-based Typst development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages =
+              with pkgs;
+              [
+                typst
+                typstyle
+                tinymist
+                self.formatter.${system}
+              ]
+              ++ (with typstPackages; [
+                # Typst packages
+              ]);
+          };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
